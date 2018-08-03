07ac1b942ae7e729deec68d62f1170bcd650cdc0
Sebastian Klingler <sklingler@pier1.com>
2018-08-03 15:39:36 -0500

    Add syntax checking layer

--- init.el
+++ init.el
@@ -51,7 +51,7 @@ values."
             shell-default-height 30
             shell-default-position 'bottom)
      ;; spell-checking
-     ;; syntax-checking
+     syntax-checking
      ;; version-control
      theming
      )
